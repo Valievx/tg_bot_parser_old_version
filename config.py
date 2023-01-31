bfa0f3dc696773407f957d7954da72e23485ed4c
Alexander Valiev <118052965+Valievx@users.noreply.github.com>
2023-01-31 20:53:59 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+token = ''
+API_ID = ''
+API_HASH = ''
+SESSION = ''
+DESTINATION = '@search22374_bot'  # получатель
+
+# чаты
+'''
+-1001369370434: 'Крипто Нарния (18+)',
+-1001791550555: 'Christian Crypto Chat',
+-1001401070472: 'DropsEarn RU',
+-1001455862301: 'Cosmos Ecosystem на Русском',
+-1001156796724: 'OTC ICOBOG',
+-1001496973960: 'OTC SWAPS',
+-1001275838415: 'Coinlist RU Community',
+'''
